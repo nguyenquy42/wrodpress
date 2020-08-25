68b56e94d2ed8c6dbddce97b787e87525ded5d98
nguyenquy42 <nguyenquy26012014@gmail.com>
2020-08-25 15:06:10 +0700

    up get_posts in index.php

--- wp-content/themes/dichvuseos/comments.php
+++ wp-content/themes/dichvuseos/comments.php
@@ -1,23 +1,4 @@
 <?php
-/**
- * The template for displaying comments
- *
- * This is the template that displays the area of the page that contains both the current comments
- * and the comment form.
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Seventeen
- * @since Twenty Seventeen 1.0
- * @version 1.0
- */
-
-/*
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
 if ( post_password_required() ) {
 	return;
 }
@@ -59,19 +40,13 @@ if ( post_password_required() ) {
 						'avatar_size' => 100,
 						'style'       => 'ol',
 						'short_ping'  => true,
-						'reply_text'  => twentyseventeen_get_svg( array( 'icon' => 'mail-reply' ) ) . __( 'Reply', 'twentyseventeen' ),
 					)
 				);
 			?>
 		</ol>
 
 		<?php
-		the_comments_pagination(
-			array(
-				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous', 'twentyseventeen' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-			)
-		);
+	
 
 	endif; // Check for have_comments().
 
@@ -83,7 +58,8 @@ if ( post_password_required() ) {
 		<?php
 	endif;
 
-	comment_form();
+	comment_form(['title_reply'=>'Comment ở đây'
+	]);
 	?>
 
 </div><!-- #comments -->
--- wp-content/themes/dichvuseos/index.php
+++ wp-content/themes/dichvuseos/index.php
@@ -78,31 +78,27 @@
         </div>
         
         <div class="row">
-        <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
-          <div class="col-lg-4 col-md-6 col-sm-12 col-12">
-            <div class="blog-item-wrapper">
-              <div class="blog-item-img">
-                <a href="<?php echo get_post_field( 'post_name' ); ?>"><?=get_the_post_thumbnail( get_the_ID(),[330,185]); ?></a>
-              </div>
-              <div class="blog-item-text box-shadow">
-                <a href="<?php echo get_post_field( 'post_name' ); ?>"> 
-                  <h5>
-                    <?php the_title();?>
-                  </h5>
-                </a>
-                <p><?=substr(strip_tags(get_the_content()),0,100); ?></p>
-                <p><?php echo get_the_date();?> | By: <?php echo get_the_author();?></p>
-              </div>
+        <?php foreach(get_posts(['numberposts'=>get_option('new_option_name')]) as $item) :?>
+            <div class="col-lg-4 col-md-6 col-sm-12 col-12">
+                <div class="blog-item-wrapper">
+                    <div class="blog-item-img">
+                        <a href="<?php echo get_post_field( 'post_name' ); ?>"><img src="<?=get_the_post_thumbnail_url($item); ?>" alt=""></a>
+                    </div>
+                    <div class="blog-item-text box-shadow">
+                        <a href="<?php echo get_post_field( 'post_name' ); ?>"> 
+                        <h5><?=$item->post_title?></h5>
+                        </a>
+                        <p><?=substr(strip_tags($item->post_content),0,100); ?></p>
+                        <p><?php echo get_the_date();?> | By: <?php echo get_the_author();?></p>
+                    </div>
+                </div>
             </div>
-          </div>
-        <?php endwhile; endif; ?>
+        <?php endforeach;  ?>
         </div>
         
       </div>
     </section>
     <!--  Useful Link Area End -->
   </main>
-
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
